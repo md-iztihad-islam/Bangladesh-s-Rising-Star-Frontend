2291a21c6fc234ac76f7c114979e676f45b4ea63
Md. Iztihad Islam <iiztihad@gmail.com>
2025-03-27 20:02:05 +0600

    bug fixed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,7 +11,7 @@ function About(){
     }, [aboutData]);
 
     return(
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center mb-10">
             <div className="flex justify-center items-center max-w-4xl w-full">
                 <div className="flex flex-col gap-5">
                     <h1 className="text-xl md:text-3xl font-bold ml-5 mt-5 mb-5">About</h1>
