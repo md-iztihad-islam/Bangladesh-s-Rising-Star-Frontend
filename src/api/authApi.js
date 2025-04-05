aa9c2a240db15eb19f40f867aae9eaea2dbad10e
Md. Iztihad Islam <iiztihad@gmail.com>
2025-04-05 22:18:29 +0600

    bug fixed

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userLoggedIn, userLoggedOut } from "./authSlice";
 
-//const PLAYERS_API = 'https://api.bangladeshsrisingstars.com/api/v1/players';
-const PLAYERS_API = 'http://localhost:8000/api/v1/players';
+const PLAYERS_API = 'https://api.bangladeshsrisingstars.com/api/v1/players';
+//const PLAYERS_API = 'http://localhost:8000/api/v1/players';
 
 export const authApi = createApi({
     reducerPath: "authApi",
